b1c08d7642a47060c75a8d378ead1ea5fce5766d
Mihai CICU <T0232181@ad-01.ent-01.adgroup>
2019-11-05 18:42:21 +0200

    testing routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RollComponent } from './roll/roll.component';
+import { TutorComponent } from './tutor/tutor.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RollComponent
+    RollComponent,
+    TutorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: RollComponent },
+      { path: 'tutors/:tutor', component: TutorComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
